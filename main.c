f7fd99ad9dec0dd3a87d1fa7aa5768095d7b7af1
Costas Chatzopoulos <kwstarikanos@gmail.com>
2019-05-18 22:09:20 +0300

    command arguments.

--- main.c
+++ main.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main() {
-    printf("Hello, World!\n");
+void wrongOptionValue(char *opt, char *val) {
+    fprintf(stderr, "\nWrong value [%s] for option '%s'\n", val, opt);
+    exit(EXIT_FAILURE);
+}
+
+/**
+ * Read options from command line*/
+void readOptions(int argc, char **argv, int *portNum) {
+    int i;
+    char *opt, *optVal;
+    for (i = 1; i < argc; ++i) {
+        opt = argv[i];
+        optVal = argv[i + 1];
+        if (strcmp(opt, "-p") == 0) {
+            if (optVal != NULL && optVal[0] != '-') {
+                *portNum = (int) strtol(optVal, NULL, 10);
+            } else {
+                wrongOptionValue(opt, optVal);
+            }
+        }
+    }
+}
+
+int main(int argc, char *argv[]) {
+    int portNum = 0;
+
+    /* Read argument options from command line*/
+    readOptions(argc, argv, &portNum);
+
+    printf("portNum: %d\n", portNum);
+
     return 0;
 }
