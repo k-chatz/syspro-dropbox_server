52864992939a01f639add6bb93d3c9e8c7769b8b
Costas Chatzopoulos <kwstarikanos@gmail.com>
2019-05-23 06:02:32 +0300

    connection helper struct for buffered requests, receive socket buf size, request buffer.

--- main.c
+++ main.c
@@ -13,9 +13,16 @@
 
 #define COLOR "\x1B[33m"
 #define RESET "\x1B[0m"
+#define MAX_CONNECTIONS 20
+
+typedef struct con {
+    size_t size;
+    void *buffer;
+    int chunks;
+} con;
 
 typedef struct client {
-    struct in_addr ip;
+    in_addr_t ip;
     in_port_t port;
 } Client;
 
@@ -54,15 +61,15 @@ void sig_int_quit_action(int signal) {
 
 int main(int argc, char *argv[]) {
     int opt = 1, fd_server = 0, fd_client = 0, activity = 0, fd_hwm = 0, fd = 0, hostname = 0;
-    void *buffer = NULL;
     static struct sigaction quit_action;
     struct sockaddr *server_ptr = NULL, *client_ptr = NULL;
     struct sockaddr_in server, client;
     struct hostent *host_entry;
     char *ip = NULL, hostbuffer[256];
-    size_t socket_size = 0;
-    socklen_t st_len = 0;
-    socklen_t client_len = 0;
+    con con_buffer[MAX_CONNECTIONS];
+    void *rcv_buffer = NULL;
+    size_t socket_rcv_size = 0, socket_snd_size = 0;
+    socklen_t st_rcv_len = 0, st_snd_len = 0, client_len = 0;
     uint16_t portNum = 0;
     ssize_t bytes = 0;
     fd_set set, read_fds;
@@ -70,7 +77,9 @@ int main(int argc, char *argv[]) {
     /* Read argument options from command line*/
     readOptions(argc, argv, &portNum);
 
-    st_len = sizeof(socket_size);
+    st_rcv_len = sizeof(socket_rcv_size);
+    st_snd_len = sizeof(socket_snd_size);
+
     client_len = sizeof(struct sockaddr);
 
     server_ptr = (struct sockaddr *) &server;
@@ -107,12 +116,19 @@ int main(int argc, char *argv[]) {
 
     ip = inet_ntoa(*((struct in_addr *) host_entry->h_addr_list[0]));
 
-    getsockopt(fd_server, SOL_SOCKET, SO_RCVBUF, (void *) &socket_size, &st_len);
+    getsockopt(fd_server, SOL_SOCKET, SO_RCVBUF, (void *) &socket_rcv_size, &st_rcv_len);
 
-    buffer = malloc(socket_size + 1);
+    getsockopt(fd_server, SOL_SOCKET, SO_SNDBUF, (void *) &socket_snd_size, &st_snd_len);
+
+    rcv_buffer = malloc(socket_rcv_size + 1);
 
     /* Config*/
     if (setsockopt(fd_server, SOL_SOCKET, SO_REUSEADDR, (char *) &opt, sizeof(opt)) < 0) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+
+    if (setsockopt(fd_server, SOL_SOCKET, SO_RCVLOWAT, (char *) &opt, sizeof(opt)) < 0) {
         perror("setsockopt");
         exit(EXIT_FAILURE);
     }
@@ -134,8 +150,11 @@ int main(int argc, char *argv[]) {
     }
 
     FD_ZERO(&set);
-
     FD_SET(fd_server, &set);
+
+    for (int i = 0; i < MAX_CONNECTIONS; i++) {
+        con_buffer[i].buffer = NULL;
+    }
 
     printf("Waiting for connections on %s:%d ... \n", ip, portNum);
     while (!quit) {
@@ -152,49 +171,68 @@ int main(int argc, char *argv[]) {
             perror("select");
         }
 
-        for (fd = 0; fd <= fd_hwm; fd++) {
+        for (fd = 0; fd <= fd_hwm && fd <= MAX_CONNECTIONS; fd++) {
             if (FD_ISSET(fd, &read_fds)) {
                 if (fd == fd_server) {
                     if ((fd_client = accept(fd_server, client_ptr, &client_len)) < 0) {
                         perror("accept");
                         break;
                     }
-                    printf("::Accept %s:%d on socket %d::\n",
-                           inet_ntoa(client.sin_addr),
-                           ntohs(client.sin_port),
+                    printf("\n::Accept %s:%d on socket %d::\n", inet_ntoa(client.sin_addr), ntohs(client.sin_port),
                            fd_client);
-
                     FD_SET(fd_client, &set);
                     if (fd_client > fd_hwm) {
                         fd_hwm = fd_client;
                     }
+                    con_buffer[fd_client].buffer = malloc(1);
+                    con_buffer[fd_client].size = 1;
+                    con_buffer[fd_client].chunks = 0;
                 } else {
-                    bytes = recv(fd_client, buffer, socket_size, 0);
-                    printf("::Receive %ld bytes from socket %d::\n", bytes, fd_client);
+                    bzero(rcv_buffer, socket_rcv_size);
+                    bytes = recv(fd, rcv_buffer, socket_rcv_size, MSG_DONTWAIT);
+                    printf("::Receive %ld bytes from socket %d::\n", bytes, fd);
                     if (bytes == 0) {
+                        shutdown(fd_client, SHUT_RD);
+
+                        printf(COLOR"\n%s\n"RESET"\n", (char *) con_buffer[fd].buffer);
+
+                        // TODO: Create command handler function.
+                        if (strncmp(con_buffer[fd].buffer, "LOG_ON", 6) == 0) {
+                            printf("EXECUTE LOG_ON\n");
+                            send(fd, "+", 1, 0);
+                            shutdown(fd_client, SHUT_WR);
+                        } else if (strncmp(con_buffer[fd].buffer, "GET_CLIENTS", 11) == 0) {
+                            printf("EXECUTE GET_CLIENTS\n");
+                            send(fd, "+", 1, 0);
+                            shutdown(fd_client, SHUT_WR);
+                        } else if (strncmp(con_buffer[fd].buffer, "LOG_OFF", 7) == 0) {
+                            printf("EXECUTE LOG_OFF\n");
+                            send(fd, "+", 1, 0);
+                            shutdown(fd_client, SHUT_WR);
+                        } else {
+                            printf("UNKNOWN COMMAND\n");
+                            send(fd, "-", 1, 0);
+                            shutdown(fd_client, SHUT_WR);
+                        }
+
                         FD_CLR(fd, &set);
                         if (fd == fd_hwm) {
                             fd_hwm--;
                         }
                         close(fd);
+                        free(con_buffer[fd].buffer);
+                        con_buffer[fd].buffer = NULL;
                     } else if (bytes > 0) {
-                        printf(COLOR"%s"RESET"\n", (char *) buffer);
-
-                        if (strncmp(buffer, "LOG_ON", 7) == 0) {
-                            printf("LOG_ON\n");
-                        } else if (strncmp(buffer, "GET_CLIENTS", 11) == 0) {
-                            printf("GET_CLIENTS\n");
-                        } else if (strncmp(buffer, "LOG_OFF", 7) == 0) {
-                            printf("LOG_OFF\n");
-                        } else {
-                            printf("UNKNOWN COMMAND\n");
-                        }
-
-                        send(fd, "OK", strlen("OK"), 0);
+                        size_t offset = con_buffer[fd].chunks ? con_buffer[fd].size - 1 : 0;
+                        printf(COLOR"%s"RESET"\n", (char *) rcv_buffer);
+                        con_buffer[fd].buffer = realloc(con_buffer[fd].buffer, con_buffer[fd].size + bytes - 1);
+                        strncpy(con_buffer[fd].buffer + offset, rcv_buffer, (size_t) bytes);
+                        con_buffer[fd].size += bytes;
+                        con_buffer[fd].chunks++;
+                        send(fd, "|", 1, 0);
                     } else {
                         perror("recv");
                     }
-
                 }
             }
         }
